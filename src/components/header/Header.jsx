cca536be649c934121c5522039f425ae18ff02e4
BrunoQ <brn.quinta@gmail.com>
2026-03-02 16:50:55 -0300

    style: add header layout and styles

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,18 +1,41 @@
 import logoBlue from "../../images/logoWhite.svg";
-
+import profileIcon from "../../images/profileIcon.png";
 
 function Header() {
   return (
-<header>
-    <div className="header">
-        <div className="header__content">
-            <div className="header__content_logo">
-                <img src={logoBlue} alt="logo plotpass" className="header__logo" />
+    <header className="header">
+      <div className="header__content">
+
+        <img 
+          src={logoBlue} 
+          alt="PlotPass logo" 
+          className="header__logo" 
+        />
+
+        <div className="header__profile">
+          <button className="header__profile-button">
+            <img 
+              src={profileIcon} 
+              alt="Perfil do usuário" 
+              className="header__profile-icon"
+            />
+          </button>
+
+          <ul className="header__profile-menu">
+            <li className="header__user-email">
+              email@email.com
+            </li>
+            <li>
+              <button className="header__logout-btn">
+                Sair
+              </button>
+            </li>
+          </ul>
+
         </div>
-            <h2>PlotPass</h2>
-        </div>
+
       </div>
-</header>
+    </header>
   );
 }
 
