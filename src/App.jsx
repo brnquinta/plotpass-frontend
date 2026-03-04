7a64efae82f5be7e28a33e8c6dcbaae92cf27891
BrunoQ <brn.quinta@gmail.com>
2026-03-04 14:24:52 -0300

    feature: Register page

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,20 @@
 import { useState } from "react";
 import { Routes, Route, Navigate, useNavigate } from "react-router-dom";
+
 import Main from "./components/main/Main";
 import PanelSearch from "./components/main/components/panels/PanelSearch";
 import RecommendationsPanel from "./components/main/components/panels/RecommendationsPanel";
 import SentRecommendationsPanel from "./components/main/components/panels/SentRecommendationsPanel";
 import AboutPanel from "./components/main/components/panels/AboutPanel";
 
+import Login from "./components/register/login/Login";
+import Signup from "./components/register/signup/Signup";
+
 function App() {
   const [search, setSearch] = useState("");
   const [query, setQuery] = useState("fight club");
 
-  const navigate = useNavigate(); 
+  const navigate = useNavigate();
 
   const handleSubmitSearch = (e) => {
     e.preventDefault();
@@ -19,20 +23,64 @@ function App() {
   };
 
   return (
-    <Main
-      search={search}
-      onSearchChange={setSearch}
-      onSubmitSearch={handleSubmitSearch}
-    >
-      {/* renderização do painel agora é por rotas */}
-      <Routes>
-        <Route path="/" element={<PanelSearch query={query} />} />
-        <Route path="/recommended" element={<RecommendationsPanel />} />
-        <Route path="/sent" element={<SentRecommendationsPanel />} />
-        <Route path="/about" element={<AboutPanel />} />
-        <Route path="*" element={<Navigate to="/" replace />} /> 
-      </Routes>
-    </Main>
+    <Routes>
+      {/* REGISTER */}
+      <Route path="/login" element={<Login />} />
+      <Route path="/signup" element={<Signup />} />
+
+      {/* APP */}
+      <Route
+        path="/"
+        element={
+          <Main
+            search={search}
+            onSearchChange={setSearch}
+            onSubmitSearch={handleSubmitSearch}
+          >
+            <PanelSearch query={query} />
+          </Main>
+        }
+      />
+      <Route
+        path="/recommended"
+        element={
+          <Main
+            search={search}
+            onSearchChange={setSearch}
+            onSubmitSearch={handleSubmitSearch}
+          >
+            <RecommendationsPanel />
+          </Main>
+        }
+      />
+      <Route
+        path="/sent"
+        element={
+          <Main
+            search={search}
+            onSearchChange={setSearch}
+            onSubmitSearch={handleSubmitSearch}
+          >
+            <SentRecommendationsPanel />
+          </Main>
+        }
+      />
+      <Route
+        path="/about"
+        element={
+          <Main
+            search={search}
+            onSearchChange={setSearch}
+            onSubmitSearch={handleSubmitSearch}
+          >
+            <AboutPanel />
+          </Main>
+        }
+      />
+
+      {/* fallback */}
+      <Route path="*" element={<Navigate to="/" replace />} />
+    </Routes>
   );
 }
 
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,39 +1,43 @@
+import { useNavigate } from "react-router-dom";
+
 import logoBlue from "../../images/logoBlue.svg";
 import profileIcon from "../../images/profileIcon.png";
 
 function Header() {
+  const navigate = useNavigate();
+
+  const handleLogout = () => {
+    navigate("/login");
+  };
+
   return (
     <header className="header">
       <div className="header__content">
-
-        <img 
-          src={logoBlue} 
-          alt="PlotPass logo" 
-          className="header__logo" 
-        />
+        <img src={logoBlue} alt="PlotPass logo" className="header__logo" />
 
         <div className="header__profile">
-          <button className="header__profile-button">
-            <img 
-              src={profileIcon} 
-              alt="Perfil do usuário" 
+          <button className="header__profile-button" type="button">
+            <img
+              src={profileIcon}
+              alt="Perfil do usuário"
               className="header__profile-icon"
             />
           </button>
 
           <ul className="header__profile-menu">
-            <li className="header__user-email">
-              email@email.com
-            </li>
+            <li className="header__user-email">email@email.com</li>
+
             <li>
-              <button className="header__logout-btn">
+              <button
+                className="header__logout-btn"
+                type="button"
+                onClick={handleLogout}
+              >
                 Sair
               </button>
             </li>
           </ul>
-
         </div>
-
       </div>
     </header>
   );
